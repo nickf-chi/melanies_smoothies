7caa521e42547ef384eb251393cfd6ae9de11d65
nickf-chi <58491117+nickf-chi@users.noreply.github.com>
2025-12-04 16:28:21 -0600

    Update streamlit_app.py
    fix typo bug

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,7 +22,7 @@ my_dataframe = session.table("smoothies.public.fruit_options").select(col('SEARC
 # Convert the snowpark dataframe to pandas dataframe so we can use the LOC function
 pd_df=my_dataframe.to_pandas()
 st.dataframe(pd_df)
-st.stop(0
+st.stop()
 
 
 ingredients_list = st.multiselect(
